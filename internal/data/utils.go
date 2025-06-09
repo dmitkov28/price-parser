e02c9f7c2d4ce2344705069eba903c774a3929ef
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-06-09 14:48:25 +0300

    feat: implement data fetching utility and product model

---
+++ internal/data/utils.go
@@ -0,0 +1,37 @@
+package data
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch data: %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+type Product struct {
+	Name string
+	Description string
+	Price float64
+	URL string
+}
+
+type ProductAdapter interface {
+    Transform(rawData []byte) (*Product, error)
+}
